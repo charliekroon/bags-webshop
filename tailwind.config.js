d87d8ce556da465955edac4ffa310e5df11c4029
charliekroon <charlotkroon@protonmail.com>
2024-03-02 16:43:20 +0100

    Fix confix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "./src/app/**/*.{js,jsx,ts,tsx}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", "./src/components/**/*.{js,jsx,ts,tsx}", "./src/app/styles/**/*.{css,scss}", "./src/app/globals.css"],
 	theme: {
 		extend: {
 			fontFamily: {
