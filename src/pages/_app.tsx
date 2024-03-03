3c619216d1cf605139599d13e359159fbb8add54
charliekroon <charlotkroon@protonmail.com>
2024-03-03 12:23:08 +0100

    Add Header to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../globals.css";
+import {Header} from "../app/components/navigation/Header";
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Header />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
