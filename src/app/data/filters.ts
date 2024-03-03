281a3d268ec08b508b2b6c63ab20dcb71d3906c7
charliekroon <charlotkroon@protonmail.com>
2024-03-03 12:31:12 +0100

    Temporary way of storing data

---
+++ src/app/data/filters.ts
@@ -0,0 +1,39 @@
+const CATEGORIES = [
+	{
+		name: "Louis Vuitton",
+	},
+	{
+		name: "Chanel",
+	},
+	{
+		name: "Gucci",
+	},
+	{
+		name: "Celine",
+	},
+];
+
+const COLORS = [
+	{
+		name: "Black",
+	},
+	{
+		name: "White",
+	},
+	{
+		name: "Multicolour",
+	},
+	{
+		name: "Brown",
+	},
+	{
+		name: "Pink",
+	},
+	{
+		name: "Red",
+	},
+];
+
+const PRICE_RANGE = [0, 1000];
+
+export {CATEGORIES, COLORS, PRICE_RANGE};
---
+++ src/app/data/products.ts
@@ -0,0 +1,36 @@
+const PRODUCTS = [
+	{
+		id: 1,
+		title: "Louis Vuitton Noe",
+		category: "Louis Vuitton",
+		imgUrl: "/public/images/products/louis-vuitton-noe.jpeg",
+		price: "780",
+		description: "Blablabla",
+	},
+	{
+		id: 2,
+		title: "Louis Vuitton Speedy 30",
+		category: "Louis Vuitton",
+		imgUrl: "/public/images/products/lv-speedy.jpeg",
+		price: "800",
+		description: "blablabla.",
+	},
+	{
+		id: 3,
+		title: "Louis Vuitton Croissant",
+		category: "Louis Vuitton",
+		imgUrl: "/public/images/products/lv-croissant.jpeg",
+		price: "800",
+		description: "lablablabal",
+	},
+	{
+		id: 4,
+		title: "Gucci Bamboo",
+		category: "Gucci",
+		imgUrl: "/public/images/products/gucci-bamboo.jpeg",
+		price: "250",
+		description: "lablablabal",
+	},
+];
+
+export {PRODUCTS};
