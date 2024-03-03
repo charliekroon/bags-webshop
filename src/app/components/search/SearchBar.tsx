bd4a8cc67dc53655f58d5d9fd554d9c275289c75
charliekroon <charlotkroon@protonmail.com>
2024-03-03 12:20:40 +0100

    Add _app.tsx

--- src/app/components/search/SearchBar.tsx
+++ src/app/components/search/SearchBar.tsx
@@ -9,12 +9,12 @@ export const SearchBar: React.FC = () => {
 			<input
 				type="text"
 				placeholder="Search"
-				className="bg-slate-300 p2 rounder-md"
+				className="bg-slate-300 p2 rounded-md"
 				value={search}
 				onClick={e => setSearch(e.target.value)}
 			/>
-			<button className="transparent border border-2 border-black w-10 h-10 mx-4 flex items-center justify-center">
-				Search
+			<button className="transparent border-2 border-black w-10 h-10 mx-4 flex items-center justify-center">
+				<Search />
 			</button>
 		</div>
 	);
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,14 +1,14 @@
 import {SearchBar} from "../app/components/search/SearchBar";
 
-const search: React.FC = () => {
+const SearchPage: React.FC = () => {
 	return (
 		<div>
 			<h1 className="text-xl p-4 text-center font-montserratRegular">
-				Search page
+				Search
 			</h1>
 			<SearchBar />
 		</div>
 	);
 };
 
-export default search;
+export default SearchPage;
